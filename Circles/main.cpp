c694641ab3ddd70509f2c2eed177a6697b1175c1
Stas <stas.shc@gmail.com>
2024-06-01 22:02:02 +0300

    add file in args

--- Circles/main.cpp
+++ Circles/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
+int main(int argc, char* argv[]) {
 
-
+	std::cout << argv[1];
+	std::ifstream in(argv[1]);
+	
+	if (in.is_open()) {
+		std::cout << "Open!\n";
+	}
+	else {
+		std::cout << "Not Open\n";
+	}
 
 	return 0;
 }
